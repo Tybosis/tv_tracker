608cab8eecb7cb86a5bba2d0faa751eeaaa07196
Tyler Pottle <tyler.pottle@gmail.com>
2015-03-24 08:23:31 -0700

    add authentication to shows controller and add seeds

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,5 +1,6 @@
 class ShowsController < ApplicationController
   before_action :set_show, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /shows
   # GET /shows.json
