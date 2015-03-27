0831380617615217f8c233380be93373e34e4155
Tyler Pottle <tyler.pottle@gmail.com>
2015-03-26 22:28:52 -0700

    increase test coverage and remove duplicate code

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,6 +1,6 @@
 class ShowsController < ApplicationController
   require 'date'
-  before_action :set_show, only: [:show, :edit, :update, :destroy]
+  before_action :set_show, only: [:show]
   before_action :authenticate_user!
   # GET /shows
   # GET /shows.json
@@ -20,15 +20,6 @@ class ShowsController < ApplicationController
     @profile = Profile.find(session[:profile_id])
   end
 
-  # GET /shows/new
-  def new
-    @show = Show.new
-  end
-
-  # GET /shows/1/edit
-  def edit
-  end
-
   # POST /shows
   # POST /shows.json
   def create
@@ -41,30 +32,6 @@ class ShowsController < ApplicationController
         format.html { render :new }
         format.json { render json: @show.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /shows/1
-  # PATCH/PUT /shows/1.json
-  def update
-    respond_to do |format|
-      if @show.update(show_params)
-        format.html { redirect_to @show, notice: 'Show was successfully updated.' }
-        format.json { render :show, status: :ok, location: @show }
-      else
-        format.html { render :edit }
-        format.json { render json: @show.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /shows/1
-  # DELETE /shows/1.json
-  def destroy
-    @show.destroy
-    respond_to do |format|
-      format.html { redirect_to shows_url, notice: 'Show was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
