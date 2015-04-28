ec3ced60795a981aa2d4eb5fff48f960325dabe3
Tyler Pottle <tyler.pottle@gmail.com>
2015-04-28 16:16:34 -0700

    setup email to be delivered in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,15 @@
 Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               "gmail.com",
+    authentication:       "plain",
+    enable_starttls_auto: true,
+    user_name:            "tvtrackerapplication@gmail.com",
+    password:             "swellfella32",
+    openssl_verify_mode:  'none'
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
