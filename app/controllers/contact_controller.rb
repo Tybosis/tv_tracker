c3981af7b689963b21c0ef117ea5af86944e28ab
Tyler Pottle <tyler.pottle@gmail.com>
2015-04-28 15:19:58 -0700

    generate mailer resource and add contact controller

---
+++ app/controllers/contact_controller.rb
@@ -0,0 +1,9 @@
+class ContactController < ApplicationController
+  def send_mail
+    name = params[:name]
+    email = params[:email]
+    body = params[:body]
+    ContactMailer.contact_email(name, email, body).deliver
+    redirect_to contact_path, notice: 'Message sent'
+  end
+end
