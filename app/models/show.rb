dd71fd2ab3b0a946845d1d942e23f633216c4e6f
Tyler Pottle <tyler.pottle@gmail.com>
2015-06-04 19:54:26 -0700

    add update series method to show model

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,10 @@
 class Show < ActiveRecord::Base
   has_and_belongs_to_many :profiles
   serialize :episodes
+
+  def update_episodes
+    show = TVDB.get_series_by_id(self.series_id)
+    self.episodes = show.episodes.last(20)
+    self.save
+  end
 end
