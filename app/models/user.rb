9d5a4a1ccdce1374ade106e70be9baefde9ba14f
Tyler Pottle <tyler.pottle@gmail.com>
2015-07-03 13:37:00 -0700

    create a default profile for the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_one :profiles
+  after_create :default_profile
+
+  def default_profile
+    @profile = Profile.create!(name: 'default profile', user_id: User.last.id)
+  end
 end
