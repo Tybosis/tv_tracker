77922b105af2a0131a456d9300016ef235ea8c4a
Matt Nickel <matt@nickelimages.com>
2015-07-04 13:16:38 -0700

    Added Calendar arrows with hover effects

--- app/assets/javascripts/addShow.js
+++ app/assets/javascripts/addShow.js
@@ -7,4 +7,13 @@ $(document).ready(function(){
       $(this).find('.plus_sign').hide();
     }
   );
+  console.log ("running")
+  $('#calendar__slideshow').hover(
+    function(){
+      $('.opacity6').show();
+    },
+    function(){
+      $('.opacity6').hide();
+    }
+  );
 });
