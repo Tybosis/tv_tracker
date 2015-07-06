922760877ed81541529207a0df436eb8f538db84
Matt Nickel <matt@nickelimages.com>
2015-07-05 22:11:35 -0700

    Finished My Shows

--- app/assets/javascripts/addShow.js
+++ app/assets/javascripts/addShow.js
@@ -7,13 +7,12 @@ $(document).ready(function(){
       $(this).find('.plus_sign').hide();
     }
   );
-  console.log ("running")
-  // $('#calendar__slideshow').hover(
-  //   function(){
-  //     $('.opacity6').show();
-  //   },
-  //   function(){
-  //     $('.opacity6').hide();
-  //   }
-  // );
+  $('.relative').hover(
+    function(){
+      $(this).find('.plus_sign2').show();
+    },
+    function(){
+      $(this).find('.plus_sign2').hide();
+    }
+  );
 });
